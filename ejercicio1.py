1040c2f2fed0d651986da66bc107a8212514b587
alexmillano <159828484+alexmillano@users.noreply.github.com>
2024-09-30 11:43:52 -0300

    Creacion de clase

---
+++ ejercicio1.py
@@ -0,0 +1,8 @@
+class Automovil{
+    def __init__(self,marca,acelaracion,color,velocidad):
+        self.marca = marca
+        self.acelaracion = acelaracion
+        self.color = color
+        self.velocidad = velocidad
+        rueda = 4
+}
