a1c97715eda405fa19431cb58ac31ac475640838
alexmillano <159828484+alexmillano@users.noreply.github.com>
2024-10-10 11:47:48 -0300

    Creacion de base de datos y archivo conexion

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from clases.vehiculo import Vehiculo
 from clases.automovil import Automovil
 from clases.automovilvolador import AutomovilVolador
+import sqlite3
 
 #def main():
     #automovil1 = Automovil('Toyota',7.5,'Negro',250)
@@ -38,3 +39,35 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+conexion = sqlite3.connect('base_datos/mi_bd.db')
+cursor = conexion.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS vehiculo (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    marca TEXT,
+    aceleracion REAL,
+    color TEXT,
+    velocidad REAL,
+    ruedas INTEGER,
+    anio TEXT,
+    modelo TEXT
+)
+''')
+
+vehiculo = ('Toyota', 7.5, 'Negro', 250, 4, '2024', 'Corolla')
+cursor.execute('''
+INSERT INTO vehiculo (marca, aceleracion, color, velocidad, ruedas, anio, modelo)
+VALUES (?, ?, ?, ?, ?, ?, ?)
+''', vehiculo)
+
+conexion.commit()
+
+cursor.execute('SELECT * FROM vehiculo')
+vehiculos = cursor.fetchall()
+for veh in vehiculos:
+    print(veh)
+
+conexion.close()
